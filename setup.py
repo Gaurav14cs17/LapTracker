3c9427066c19329651b0297b650da798d04441f6
Adrian <aditschans@gmail.com>
2020-08-05 12:00:38 +0200

    added setup and init

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  5 11:53:11 2020
+
+@author: Adrian
+"""
+
+from distutils.core import setup
+
+setup(name='LapTracker',
+      version='1.0',
+      description='simplified linear assignment problem object tracker',
+      author='Adrian Tschan',
+      author_email='adrian.tschan@uzh.ch',
+      packages=['LapTracker'],
+      install_requires=['numpy', 'pandas', 'scipy', 'networkx', 'skimage',
+                        'progress']
+     )
